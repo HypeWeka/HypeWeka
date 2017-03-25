536f8b81477e9e22b842b26f48417a8cbd528d87
hitpointn1 <hitpoin1@gmail.com>
2017-03-25 13:49:01 +0300

    Izmenil SetMap i PrintMap, Dobavil printscore, Vivel constantno e zna4

--- Main.cpp
+++ Main.cpp
@@ -5,160 +5,234 @@
 #include <cstdlib>
 #include <conio.h>
 #include <Windows.h>
+#include <string>
 using namespace std;
-// 
-void setMap(char **k, int *h, int *w,int x,int y)
+
+const int height = 20, width = 10;
+
+void setMap(char **k, int x, int y);
+void printscore(int ikf, int scr);
+void printMap(char **m, int ks);
+void clearscreen();
+int main()
 {
- 
-    for(int i=0;i<*h;i++){
-        for(int j=0;j<*w;j++){
-            if(i==0 || i==*h-1){k[i][j] = 'w';}
-            else if(j==0 || j==*w-1){k[i][j] = 'w';}
-            else {k[i][j] = ' ';}
-        }
-    }
- 
-    k[x][y]='*';
-    k[x+1][y]='*';
-    k[x+2][y]='*';
-    k[x+1][y-1]='*';
-    k[x+1][y+1]='*';
-    k[x+3][y-1]='*';
-    k[x+3][y+1]='*';
+	int x = 8, y = 6;
+	char** map = new char*[height];
+
+	for (int i = 0; i<height; i++)
+		map[i] = new char[width];
+
+	setMap(map, x, y);
+
+	char move;
+	int kscore = 0;
+
+	while (true)
+	{
+		clearscreen();
+		printMap(map, kscore);
+		move = _getch();
+		if (move == 'w'  && x>1)
+		{
+			map[x + 1][y + 2] = ' ';
+			map[x + 1][y + 1] = ' ';
+			map[x][y] = ' ';
+			map[x + 2][y - 2] = ' ';
+			map[x + 2][y - 1] = ' ';
+			map[x + 2][y + 2] = ' ';
+			map[x + 2][y + 1] = ' ';
+			map[x + 1][y] = ' ';
+			map[x + 2][y] = ' ';
+			map[x + 1][y - 1] = ' ';
+			map[x + 3][y] = ' ';
+			map[x + 4][y] = ' ';
+			map[x + 1][y + 1] = ' ';
+			map[x + 3][y - 1] = ' ';
+			map[x + 3][y + 1] = ' ';
+			map[--x][y] = '*';
+			map[x + 1][y] = '*';
+			map[x + 2][y] = '*';
+			map[x + 1][y - 1] = '*';
+			map[x + 1][y + 1] = '*';
+			map[x + 3][y - 1] = '*';
+			map[x + 3][y + 1] = '*';
+		}
+		if (move == 's' && x<height - 6)
+		{
+			map[x][y] = ' ';
+			map[x + 2][y - 2] = ' ';
+			map[x + 2][y - 1] = ' ';
+			map[x + 2][y + 2] = ' ';
+			map[x + 2][y + 1] = ' ';
+			map[x + 1][y + 2] = ' ';
+			map[x + 1][y + 1] = ' ';
+			map[x + 1][y] = ' ';
+			map[x + 2][y] = ' ';
+			map[x + 1][y - 1] = ' ';
+			map[x + 1][y + 1] = ' ';
+			map[x + 3][y - 1] = ' ';
+			map[x + 3][y + 1] = ' ';
+			map[++x][y] = ' ';
+			map[x + 3][y] = '*';
+			map[x + 4][y] = '*';
+			map[x + 2][y] = '*';
+			map[x + 1][y - 1] = '*';
+			map[x + 1][y + 1] = '*';
+			map[x + 3][y - 1] = '*';
+			map[x + 3][y + 1] = '*';
+		}
+		if (move == 'd' && y<width - 5)
+		{
+			map[x + 1][y + 2] = ' ';
+			map[x + 1][y + 1] = ' ';
+			map[x][y] = ' ';
+			map[x + 2][y - 2] = ' ';
+			map[x + 2][y - 1] = ' ';
+			map[x + 1][y] = ' ';
+			map[x + 2][y] = ' ';
+			map[x + 1][y - 1] = ' ';
+			map[x + 3][y] = ' ';
+			map[x + 4][y] = ' ';
+			map[x + 1][y + 1] = ' ';
+			map[x + 3][y - 1] = ' ';
+			map[x + 3][y + 1] = ' ';
+			map[x][++y] = ' ';
+			map[x + 1][y] = ' ';
+			map[x + 2][y + 2] = '*';
+			map[x + 2][y + 1] = '*';
+			map[x + 2][y] = '*';
+			map[x + 1][y - 1] = '*';
+			map[x + 1][y + 1] = '*';
+			map[x + 3][y - 1] = '*';
+			map[x + 3][y + 1] = '*';
+		}
+
+		if (move == 'a' && y>3)
+		{
+			map[x + 1][y + 2] = ' ';
+			map[x + 1][y + 1] = ' ';
+			map[x][y] = ' ';
+			map[x + 1][y] = ' ';
+			map[x + 2][y] = ' ';
+			map[x + 1][y - 1] = ' ';
+			map[x + 3][y] = ' ';
+			map[x + 4][y] = ' ';
+			map[x + 1][y + 1] = ' ';
+			map[x + 3][y - 1] = ' ';
+			map[x + 3][y + 1] = ' ';
+			map[x][--y] = ' ';
+			map[x + 1][y] = ' ';
+			map[x + 2][y - 2] = '*';
+			map[x + 2][y - 1] = '*';
+			map[x + 2][y] = '*';
+			map[x + 1][y - 1] = '*';
+			map[x + 1][y + 1] = '*';
+			map[x + 3][y - 1] = '*';
+			map[x + 3][y + 1] = '*';
+		}
+		kscore++;
+	}
+	return 0;
 }
- 
-void printMap(char **m, int* h, int *w){
-    for(int i=0;i<*h;i++){
-        for(int j=0;j<*w;j++){
-            cout<<m[i][j];
-        }
-    cout<<endl;
-    } 
+
+void setMap(char **k, int x, int y)
+{
+	for (int i = 0; i<height; i++)
+	{
+		for (int j = 0; j<width; j++)
+		{
+			if (i == 0 || i == height - 1)
+			{
+				k[i][j] = ' ';
+				if (i == 0 && (j == 0 || j == 9))k[i][j] = '#';
+				if (i == height - 1 && (j == 0 || j == 9))k[i][j] = '#';
+			}
+			else
+				if (j == 0 || j == width - 1)
+					k[i][j] = '#';
+				else
+					k[i][j] = ' ';
+
+		}
+	}
+
+	bool f = false;
+	for (int i = 0; i < height - 1; i++)
+	{
+
+		if (i % 4 == 0 && i != 0) f = true;
+		if (f == true)
+		{
+			k[i][0] = ' '; k[i + 1][9] = ' ';
+			f = false;
+		}
+
+	}
+
+	k[x][y] = '*';
+	k[x + 1][y] = '*';
+	k[x + 2][y] = '*';
+	k[x + 1][y - 1] = '*';
+	k[x + 1][y + 1] = '*';
+	k[x + 3][y - 1] = '*';
+	k[x + 3][y + 1] = '*';
 }
-    
+void printscore(int ikf, int scr)
+{
+	string s1 = " 61th R O A D  R A C E ";
+	string sr = "=======================";
+	string s2 = " S C O R E:";
+	string sp = "  + + + + + + + + + +";
+	switch (ikf)
+	{
+	case 0:
+		cout << ' ' << sr;
+		break;
+	case 1:
+		cout << ' ' << s1;
+		break;
+	case 2:
+		cout << ' ' << sr;
+		break;
+	case 7:
+		cout << sp;
+		break;
+	case 8:
+		cout << "  +" << s2 << ' ' << scr;
+		if (scr < 10)cout << "    +";
+		else
+			if (scr < 100) cout << "   +";
+			else
+				if (scr < 1000) cout << "  +";
+				else
+					if (scr < 10000) cout << " +";
+					else scr = 0;
+					break;
+	case 9:
+		cout << sp;
+		break;
+	}
+}
+
+void printMap(char **m, int ks)
+{
+	for (int i = 0; i<height; i++)
+	{
+		for (int j = 0; j<width; j++)
+		{
+			cout << m[i][j];
+		}
+		printscore(i, ks);
+		cout << endl;
+	}
+}
+
 void clearscreen()
 {
-    HANDLE hOut;
-    COORD Position;
-    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    Position.X = 0;
-    Position.Y = 0;
-    SetConsoleCursorPosition(hOut, Position);
-}
-int main(){
-    int height, width;
-    cout<<"Enter height and width of a map"<<endl;
-    cin>>height>>width;
-    int x=8,y=6;
-    char** map = new char* [height];
-    for(int i=0;i<height;i++){
-        map[i] = new char[width];
-    }
-    setMap(map, &height, &width, x, y);
-    char move;
-    while(true)
-        {
-        clearscreen();
-        printMap(map,&height, &width);
-        move=_getch();
-        if(move == 'w'  && x>1)
-        {
-            map[x+1][y+2]=' ';
-            map[x+1][y+1]=' ';
-            map[x][y]=' ';
-            map[x+2][y-2]=' ';
-            map[x+2][y-1]=' ';
-            map[x+2][y+2]=' ';
-            map[x+2][y+1]=' ';
-            map[x+1][y]=' ';
-            map[x+2][y]=' ';
-            map[x+1][y-1]=' ';
-            map[x+3][y]=' ';
-            map[x+4][y]=' ';
-            map[x+1][y+1]=' ';
-            map[x+3][y-1]=' ';
-            map[x+3][y+1]=' ';
-            map[--x][y]='*';
-            map[x+1][y]='*';
-            map[x+2][y]='*';
-            map[x+1][y-1]='*';
-            map[x+1][y+1]='*';
-            map[x+3][y-1]='*';
-            map[x+3][y+1]='*';
-        }
-        if(move=='s' && x<height-6)
-{
-    map[x][y]=' ';
-    map[x+2][y-2]=' ';
-    map[x+2][y-1]=' ';
-    map[x+2][y+2]=' ';
-    map[x+2][y+1]=' ';
-    map[x+1][y+2]=' ';
-    map[x+1][y+1]=' ';
-    map[x+1][y]=' ';
-    map[x+2][y]=' ';
-    map[x+1][y-1]=' ';
-    map[x+1][y+1]=' ';
-    map[x+3][y-1]=' ';
-    map[x+3][y+1]=' ';
-    map[++x][y]=' ';
-    map[x+3][y]='*';
-    map[x+4][y]='*';
-    map[x+2][y]='*';
-    map[x+1][y-1]='*';
-    map[x+1][y+1]='*';
-    map[x+3][y-1]='*';
-    map[x+3][y+1]='*';
-        }
-        if(move=='d' && y<width-5)
-{
-    map[x+1][y+2]=' ';
-    map[x+1][y+1]=' ';
-    map[x][y]=' ';
-    map[x+2][y-2]=' ';
-    map[x+2][y-1]=' ';
-    map[x+1][y]=' ';
-    map[x+2][y]=' ';
-    map[x+1][y-1]=' ';
-    map[x+3][y]=' ';
-    map[x+4][y]=' ';
-    map[x+1][y+1]=' ';
-    map[x+3][y-1]=' ';
-    map[x+3][y+1]=' ';
-    map[x][++y]=' ';
-    map[x+1][y]=' ';
-    map[x+2][y+2]='*';
-    map[x+2][y+1]='*';
-    map[x+2][y]='*';
-    map[x+1][y-1]='*';
-    map[x+1][y+1]='*';
-    map[x+3][y-1]='*';
-    map[x+3][y+1]='*';
-        }
- 
-        if(move=='a' && y>3)
-        {
-            map[x+1][y+2]=' ';
-            map[x+1][y+1]=' ';
-            map[x][y]=' ';
-            map[x+1][y]=' ';
-            map[x+2][y]=' ';
-            map[x+1][y-1]=' ';
-            map[x+3][y]=' ';
-            map[x+4][y]=' ';
-            map[x+1][y+1]=' ';
-            map[x+3][y-1]=' ';
-            map[x+3][y+1]=' ';
-            map[x][--y]=' ';
-            map[x+1][y]=' ';
-            map[x+2][y-2]='*';
-            map[x+2][y-1]='*';
-            map[x+2][y]='*';
-            map[x+1][y-1]='*';
-            map[x+1][y+1]='*';
-            map[x+3][y-1]='*';
-            map[x+3][y+1]='*';
-        }
-   }
-   return 0;
+	HANDLE hOut;
+	COORD Position;
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	Position.X = 0;
+	Position.Y = 0;
+	SetConsoleCursorPosition(hOut, Position);
 }
